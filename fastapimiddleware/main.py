6e664c30ababf5da3b3630ded17b68773f759df2
Manikanta9959 <sivachallano1@gmail.com>
2025-01-30 22:45:39 +0530

    fastapimiddlewares

--- fastapimiddleware/main.py
+++ fastapimiddleware/main.py
@@ -1,9 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Response
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.middleware.gzip import GZipMiddleware
+from fastapi.responses import JSONResponse
+from starlette.middleware.base import BaseHTTPMiddleware
 
 app = FastAPI()
 
+#cors middleware
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -12,11 +15,31 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# custom middleware for logging req and res
+class Logging(BaseHTTPMiddleware):
+    async def dispatch(self, request : Request, call_next):
+        print(">>>>>>>>>0998>>>>>>>0998>>>>>>>0998>",request.url)
+        response = await call_next(request)
+        return response
+   
+app.add_middleware(
+    Logging
+)
 
+#gzip middleware
 app.add_middleware(
     GZipMiddleware,
     minimum_size = 100
 )
+
+#global exception handler
+@app.exception_handler(Exception)
+async def global_unhandled_exception(request: Request, exc: Exception):
+    return JSONResponse(
+        status_code=500,
+        content={"message": f"An unexpected error occurred. Please try again later. {exc}"},
+    )
+    
 
 
 @app.get("/")
@@ -32,3 +55,8 @@ async def gzip():
     return {
         "value" : val
     }
+    
+    
+@app.get("/cause-error")
+async def cause_error():
+    return 1 / 0  
--- readme.md
+++ readme.md
@@ -1,3 +1,11 @@
 #middleware
 used to modify requests and responses globally before they reach the endpoint or after they are processed.
 
+
+FastAPI middlewares
+```bash 
+    CORSMiddleware	Enables cross-origin requests
+    GZipMiddleware	Compresses large responses
+    TrustedHostMiddleware	Restricts access to specific domains
+    Custom Middleware	Logging, Authentication, Rate Limiting, etc.
+```
